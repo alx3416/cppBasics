e6b02daa0e61871fec279c620462b63759f19c41
alx3416 <alx3416@hotmail.com>
2023-10-03 00:01:23 -0600

    adding pass by pointer and vector timeit

--- pointers/pointers.cpp
+++ pointers/pointers.cpp
@@ -17,5 +17,25 @@ int main() {
     ptrs_basics::squarePassByPointer(&x);
     std::cout << "x after pass by pointer= " << x << std::endl;
     std::cout << "x address pass by pointer-> " << &x << std::endl;
+
+    int y = 34;
+    std::cout << "before swap, x = " << x << ", " << "y = " << y << std::endl;
+    ptrs_basics::swapByPointer(&x, &y);
+    std::cout << "after swap, x = " << x << ", " << "y = " << y << std::endl;
+
+    std::vector<int> array(100000000);
+    auto startTime = std::chrono::high_resolution_clock::now();
+    array = ptrs_basics::fillVectorByValue(array);
+    auto stopTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stopTime - startTime);
+    std::cout << "Time taken by fillVectorByValue: " << duration.count() << " microseconds" << std::endl;
+
+    std::vector<int> array2(100000000);
+    startTime = std::chrono::high_resolution_clock::now();
+    ptrs_basics::fillVectorByReference(array);
+    stopTime = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(stopTime - startTime);
+    std::cout << "Time taken by fillVectorByReference: " << duration.count() << " microseconds" << std::endl;
+
     return 0;
 }
--- pointers/pointers_utils.h
+++ pointers/pointers_utils.h
@@ -1,6 +1,10 @@
 #ifndef CPPBASICS_POINTERS_UTILS_H
 #define CPPBASICS_POINTERS_UTILS_H
 
+#include <chrono>
+#include <iostream>
+#include <vector>
+
 namespace ptrs_basics {
     int squarePassByValue(int n) {
         n *= n;
@@ -15,6 +19,25 @@ namespace ptrs_basics {
         int n = *n_ptr;
         n *= n;
         *n_ptr = n;
+    }
+
+    void swapByPointer(int *x, int *y){
+        int z = *x;
+        *x = *y;
+        *y = z;
+    }
+
+    std::vector<int> fillVectorByValue(std::vector<int> vec){
+        for(size_t i=0; i<vec.size(); i++){
+            vec[i] = static_cast<int>(i);
+        }
+        return vec;
+    }
+
+    void fillVectorByReference(std::vector<int> &vec){
+        for(size_t i=0; i<vec.size(); i++){
+            vec[i] = static_cast<int>(i);
+        }
     }
 }  // ptrs_basics
 #endif //CPPBASICS_POINTERS_UTILS_H
