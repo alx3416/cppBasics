0547f36e6fe447696bfe8183b0ce56414373b809
alx3416 <63263335+alx3416@users.noreply.github.com>
2023-10-09 14:00:24 -0600

    adding stack heap examples

--- pointers/pointers.cpp
+++ pointers/pointers.cpp
@@ -51,6 +51,32 @@ int main() {
     duration = std::chrono::duration_cast<std::chrono::microseconds>(stopTime - startTime);
     std::cout << "Time taken by fillVectorByReference: " << duration.count() << " microseconds" << std::endl;
 
+    // new and delete operators
+    // pointer-variable = new data-type(value);
+    // int *p = nullptr;
+    // p = new int;
+    // option 2
+    // int *r = new int;
+    int* p = new int(25);
+    std::cout << "p Address: " << p << std::endl;
+    std::cout << "p value: " << *p << std::endl;
+    delete p;
+
+    std::cout << "Press any key to continue\n";
+    std::cin.ignore();
+    // trying with a very large c-array
+    int length = 50000000;
+    int *pArray = new int[length];
+    for(int i = 0; i < length; i++ ){
+        pArray[i] = i;
+    }
+    /*for(int i = 0; i < length; i++ ){
+        std::cout << pArray[i] << std::endl;
+    }*/
+    std::cout << "Press any key to exit\n";
+    std::cin.ignore();
+    delete[] pArray;
+
     return 0;
-    
+
 }
