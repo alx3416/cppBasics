cb7aed627e21159248406a67606545c16f199f5d
alx3416 <63263335+alx3416@users.noreply.github.com>
2023-10-25 13:53:17 -0600

    adding first exercise templates

--- templates/template_exercises.cpp
+++ templates/template_exercises.cpp
@@ -17,12 +17,17 @@ int main(){
     delete ptr2;
 
     // swap 2 vectors v1=a,b,c  v2=x,y,z
+    std::vector<char> text1 {'a', 'b', 'c'};
+    std::vector<char> text2 {'x', 'y', 'z', 'f'};
+    myTemplates::SwapData(text1, text2);
+    std::cout << " vec1 = " << text1.at(0) << std::endl;
+    std::cout << " vec2 = " << text2.at(0) << std::endl;
 
     // sum all elements in a C Array
     float float_data[5] {3.14, -2.71, 1.33, -7.77, 5.1};
     int int_data[5] {10,20,30,40,50};
     std::cout << "Sum float_data = " << myTemplates::sum(float_data, 5) << std::endl;
     std::cout << "Sum int_data = " << myTemplates::sum(int_data, 5) << std::endl;
-    
+
     return 0;
 }
--- templates/template_exercises.h
+++ templates/template_exercises.h
@@ -10,5 +10,17 @@ namespace myTemplates {
         return (x > y) ? x : y;
     }
 
+    template<typename Type_data>
+    void SwapData (Type_data &x, Type_data &y){
+        Type_data temp;
+        temp = x;
+        x = y;
+        y = temp;
+    }
+
+    void myfun (int x[]){
+        ;
+    }
+
 } // myTemplates
 #endif //CPPBASICS_TEMPLATE_EXERCISES_H
